f479ef8ea4b4fca379f60a1c29ffc887a4152d5f
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-12-21 00:38:07 +0100

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Opt {
+    #[structopt(short, long, default_value = "800")]
+    width: usize,
+
+    #[structopt(short, long, default_value = "600")]
+    height: usize,
+
+    #[structopt(short, long, required = true, parse(from_os_str))]
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("width: {}", opt.width);
+    println!("height: {}", opt.height);
+    println!("output path: {}", opt.output.display());
 }
