d50356cbed1409b16376e223b867a734bc3054a9
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-02-10 19:58:15 +0100

    Add some gradient functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::path::PathBuf;
-
 use image::{ImageBuffer, Pixel, Rgb};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, Clone, Default, PartialEq, Eq)]
 pub struct Opt {
     #[structopt(short, long, default_value = "800")]
     width: u32,
@@ -20,11 +19,24 @@ fn main() {
 
     let mut img = ImageBuffer::new(opt.width, opt.height);
 
-    for i in 0..opt.width / 2 {
-        for j in 0..opt.height / 2 {
-            let pixel = Rgb::from_channels(i as u8, j as u8, 60, 0);
-            img.put_pixel(i as u32, j as u32, pixel);
+    for i in 0..opt.width {
+        for j in 0..opt.height {
+            img.put_pixel(i as u32, j as u32, delphi_gradient(i / 10));
         }
     }
     img.save(opt.output).unwrap();
+}
+
+fn delphi_gradient(i: u32) -> Rgb<u8> {
+    let r = (i >> 5) * 36;
+    let g = ((i >> 3) & 7) * 36;
+    let b = (i & 3) * 85;
+    Rgb::from_channels(r as u8, g as u8, b as u8, 0)
+}
+
+fn smooth_gradient(i: u32) -> Rgb<u8> {
+    let r = i * 5;
+    let g = (i + 30) * 10;
+    let b = i * 3;
+    Rgb::from_channels(r as u8, g as u8, b as u8, 0)
 }
