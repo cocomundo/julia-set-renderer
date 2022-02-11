02e6ee89619640cd9c5213a5f731bbdea713df30
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-02-11 19:36:51 +0100

    Somewhat functional version

--- src/main.rs
+++ src/main.rs
@@ -24,24 +24,33 @@ fn main() {
 
     let (move_x, move_y) = (0.0, 0.0);
     let max_iter = 255;
-    let zoom = 1.0f32;
+    let zoom = 2.0f32;
 
     let w = opt.width as f32;
     let h = opt.height as f32;
 
-    for x in 0..opt.width {
-        for y in 0..opt.height {
-            let zx = 1.5 * (x as f32 - w / 2.0) / (0.5 * zoom * w) + move_x;
-            let zy = 1.0 * (y as f32 - h / 2.0) / (0.5 * zoom * h) + move_y;
+    let outer = (0..opt.width)
+        .map(|x| {
+            (0..opt.height)
+                .map(|y| {
+                    let zx = 1.5 * (x as f32 - w / 2.0) / (0.5 * zoom * w) + move_x;
+                    let zy = 1.0 * (y as f32 - h / 2.0) / (0.5 * zoom * h) + move_y;
 
-            let i = convergence_steps(max_iter, zx, zy);
+                    convergence_steps(max_iter, zx, zy)
+                })
+                .map(|i| {
+                    let r = (i << 3) as u8;
+                    let g = (i << 5) as u8;
+                    let b = (i << 4) as u8;
+                    Rgb::from_channels(r, g, b, 0)
+                })
+                .collect::<Vec<Rgb<u8>>>()
+        })
+        .collect::<Vec<_>>();
 
-            // convert byte to RGB (3 bytes), kinda magic to get nice colors
-            let r = (i << 3) as u8;
-            let g = (i << 5) as u8;
-            let b = (i << 4) as u8;
-            let pixel = Rgb::from_channels(r, g, b, 0);
-            img.put_pixel(x as u32, y as u32, pixel);
+    for (x, row) in outer.iter().enumerate() {
+        for (y, pixel) in row.iter().enumerate() {
+            img.put_pixel(x as u32, y as u32, *pixel);
         }
     }
 
@@ -58,14 +67,14 @@ fn convergence_steps(max_iter: i32, mut zx: f32, mut zy: f32) -> i32 {
     i
 }
 
-fn delphi_gradient(i: u32) -> Rgb<u8> {
+fn delphi_gradient(i: i32) -> Rgb<u8> {
     let r = (i >> 5) * 36;
     let g = ((i >> 3) & 7) * 36;
     let b = (i & 3) * 85;
     Rgb::from_channels(r as u8, g as u8, b as u8, 0)
 }
 
-fn smooth_gradient(i: u32) -> Rgb<u8> {
+fn smooth_gradient(i: i32) -> Rgb<u8> {
     let r = i * 5;
     let g = (i + 30) * 10;
     let b = i * 3;
