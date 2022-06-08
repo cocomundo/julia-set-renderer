22dc76605663bed7187532221f0104516c6f9261
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-06-08 10:22:58 +0200

    Use default_value_t instead

--- src/lib.rs
+++ src/lib.rs
@@ -17,22 +17,22 @@ const MAX_ITER: i32 = 256;
 #[derive(Debug, Parser, Clone, Default, PartialEq)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(short, long, default_value = "800")]
+    #[clap(short, long, default_value_t = 800)]
     width: u32,
 
-    #[clap(short, long, default_value = "600")]
+    #[clap(short, long, default_value_t = 600)]
     height: u32,
 
     #[clap(short, long, parse(from_os_str), default_value = "out.png")]
     output: PathBuf,
 
-    #[clap(short, long, default_value = "1.0")]
+    #[clap(short, long, default_value_t = 1.0)]
     zoom: f64,
 
-    #[clap(short, long, allow_hyphen_values = true, default_value = "0.0")]
+    #[clap(short, long, allow_hyphen_values = true, default_value_t = 0.0)]
     x_offset: f64,
 
-    #[clap(short, long, allow_hyphen_values = true, default_value = "0.0")]
+    #[clap(short, long, allow_hyphen_values = true, default_value_t = 0.0)]
     y_offset: f64,
 
     #[clap(long, takes_value = false)]
